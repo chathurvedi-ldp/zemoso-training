1b92a4bb4b29e6cdaa79fbaab1f7ca6075c86b48
chathurvedi <jarugu.chathurvedi@zemosolabs.com>
2022-11-28 09:01:24 +0530

    after feedback

--- Themeing.tsx
+++ Themeing.tsx
@@ -1,8 +1,16 @@
 import { createTheme } from "@mui/system";
-import { purple } from "@mui/material/colors";
 
 export const theme=createTheme({
+  spacing: [0, 4, 8, 16, 32, 64],
   typography:{
+    title: {
+      fontFamily: 'Gilroy',
+      fontSize: '36px',
+      fontWeight: '700',
+      lineHeight: '42px',
+      letterSpacing: '-0.015em',
+      textAlign: 'left',
+  },
     h1:{
       fontFamily:'Gilroy',
       fontWeight:600,
@@ -45,80 +53,49 @@ export const theme=createTheme({
   },
     palette: {
       primary: {
-        purple:500,
-        white:600,
+        purple:{
+          600:'#393552',
+          500:'#6C5DD3',
+          400:'#B4A9FF',
+        },
+        white:{
+          600:'#D8D8D9',
+          500:'#E8E8E9',
+        }
       },
       background:{
-        elevation:'#2D2D30'
+        elevation2:'#2D2D30',
+        elevation1:'#201F24',
+        elevation0:'#19181C',
       },
-      grey:100,
-      text:'white',
+      grey:{
+        100:'#262529',
+        50:'#3A3A3D',
+      },
+      text:{
+        highEmp:'#FFFFFF',
+        medEmp:'#E8E7F0',
+        lowEmp:'#A5A5A6',
+      },
+      icon:{
+        highEmp:'#D4D2DE',
+        lowEmp:'#727080',
+      },
+      border:{
+        highEmp:'#413F4D',
+        lowEmp:'#28272B'
+      },
+      accent:{
+        DarkOverlay:'#333333',
+        pink:'#E39AB2',
+        aquaBlue:'#A0D7E7',
+        lightLavender:'#CFC8FF',
+        peachYellow:'#E5CB9B',
+        redOrange:'#EC977D'
+      },
       neutral: {
         main: '#64748B',
         contrastText: '#fff',
       },
     },
 })
-
-
-
-
-
-// import * as React from 'react';
-// import { styled } from '@mui/material/styles';
-// import Typography from '@mui/material/Typography';
-// import { red, green, blue } from '@mui/material/colors';
-// import { Button, createTheme, ThemeProvider } from '@mui/material';
-// import {purple} from '@mui/material/colors';
-
-// const Root = styled('div')(({ theme }) => ({
-//   padding: theme.spacing(1),
-//   [theme.breakpoints.down('md')]: {
-//     backgroundColor: red[500],
-//   },
-//   [theme.breakpoints.up('md')]: {
-//     backgroundColor: blue[500],
-//   },
-//   [theme.breakpoints.up('lg')]: {
-//     backgroundColor: green[500],
-//   },
-// }));
-
-// export default function Themeing() {
-//   const theme1 = createTheme({
-//     palette: {
-//       primary: {
-//         main: purple[500],
-//       },
-//       secondary: {
-//         main: '#11cb5f',
-//       },
-//     },
-//   });
-//   const theme2 = createTheme();
-//   theme2.typography.h3={
-//     fontSize: '1.2rem',
-//   '@media (min-width:600px)': {
-//     fontSize: '1.5rem',
-//   },
-//   [theme2.breakpoints.up('md')]: {
-//     fontSize: '2rem',
-//   },
-//   }
-//   return (
-//     <>
-//     <Root>
-//       <Typography>down(md): red</Typography>
-//       <Typography>up(md): blue</Typography>
-//       <Typography>up(lg): green</Typography>
-//     </Root><br/> <br/>
-//     <ThemeProvider theme={theme1}>
-//       <Button variant="contained">Primary</Button>
-//       <Button variant="contained" color="secondary">Secondary</Button>
-//     </ThemeProvider><br/> <br/>
-//     <ThemeProvider theme = {theme2}>
-//       <Typography variant="h3">Responsive h3</Typography>
-//     </ThemeProvider>
-//     </>
-//   );
-// }
